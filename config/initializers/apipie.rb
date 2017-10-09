1136fcf3602746117878823d29abf7c09debee34
ahmad ramdani <ahmad.ramdani@gopherindonesia.com>
2017-10-09 10:21:34 +0700

    Define API Version

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,7 +1,13 @@
 Apipie.configure do |config|
-  config.app_name                = "SocialApiTest"
+  config.app_name                = "Social Network API"
+  config.app_info                = "Social Network API Documentation RC1"
   config.api_base_url            = "/api"
-  config.doc_base_url            = "/apipie"
-  # where is your API defined?
+  config.doc_base_url            = ''
+  config.validate                = false
+  config.namespaced_resources    = true
+
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
+
+  config.copyright               = "Ahmad Ramdani 2017"
+  config.default_version         = "1.0"
 end
